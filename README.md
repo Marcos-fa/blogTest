f4f67d44ed7a4b875bf8ae9952fac00c0687f2fc
Marcos-fa <marcos.fa0216@gmail.com>
2023-12-29 09:13:23 -0700

    Updated Readme

--- README.md
+++ README.md
@@ -4,6 +4,9 @@ This is a new [**React Native**](https://reactnative.dev) project, bootstrapped 
 
 >**Note**: Make sure you have completed the [React Native - Environment Setup](https://reactnative.dev/docs/environment-setup) instructions till "Creating a new application" step, before proceeding.
 
+## Configuration for backend connection
+In file ./src/api/AppApi if you wanna run it locally uncomment line 5 and comment line 6-7
+
 ## Step 1: Start the Metro Server
 
 First, you will need to start **Metro**, the JavaScript _bundler_ that ships _with_ React Native.
--- src/api/AppApi.tsx
+++ src/api/AppApi.tsx
@@ -1,10 +1,10 @@
 import axios from 'axios';
 import {IFullPost, IPost} from 'interfaces/stores/IAppStore';
 
-// const API_URL = 'http://localhost:3000';
 // si quieres probarlo en tu celular fisico, cambia localhost por tu ip local
+// const API_URL = 'http://localhost:3000'; // Descomenta si quieres correrlo localmente y comenta la linea 6-7
 const API_URL =
-  'https://36fb-2806-2f0-20c1-f35b-b53f-521d-1b68-5747.ngrok-free.app'; // si quieres probarlo en tu celular, cambia localhost por tu ip local
+  'https://36fb-2806-2f0-20c1-f35b-b53f-521d-1b68-5747.ngrok-free.app'; // Aqui puedes poner tu ruta de ngrok
 
 export const getPosts = async (): Promise<IFullPost[]> => {
   try {
